5467c9fde02651c31f8e005877d317e9837298e9
vaishnav-rajput <vaishnav.rajput108@gmail.com>
2023-09-20 14:36:03 +0530

    model for USER

---
+++ config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("Db connection successfully"))
+    .catch((error) => {
+        console.log("Db connection failed")
+        console.error(error)
+        process.exit(1)
+    })
+}
