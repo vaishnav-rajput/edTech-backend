b87e868aaa0d56f96d23f87bb34051c94c308b8c
vaishnav-rajput <vaishnav.rajput108@gmail.com>
2023-09-29 23:58:09 +0530

    auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,3 +1,15 @@
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
+const User = require("../models/User")
+
+
 //auth
+exports.auth = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
 
 //
