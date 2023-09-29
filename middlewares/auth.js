d7b5544a783ee0a53b8d9ed8bd29b54bc3fdc834
vaishnav-rajput <vaishnav.rajput108@gmail.com>
2023-09-30 02:58:47 +0530

    isStudent isInstructor midlleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,10 +6,52 @@ const User = require("../models/User")
 //auth
 exports.auth = async (req, res, next) => {
     try {
-        
+        //extract token
+        const token = req.cookies.token || req.body.token || req.header("Authorisation").replace("Bearer", "")
+
+        //if token missing, then return response
+        if(!token){
+            return res.status(401).json({
+                success: false,
+                message: "Token is missing"
+            })
+        }
+
+        //verify the token
+        try{
+            const decode = jwt.verify(token, process.env.JWT_SECRET)
+            console.log("decode", decode)
+            req.user = decode //adding this to the req.user will help in the authorization process by further middleware
+        } catch(error) {
+            //verification - issue
+            return res.status(401).json({
+                success: false,
+                message: "TOken is invalid"
+            })
+        }
+        next()
     } catch (error) {
-        
+        return res.status(401).json({
+            success: false,
+            message: "something went wrong while validating the token"
+        })        
     }
 }
 
-//
+//isStudent
+exports.isInstructor = async (req, res, next) => {
+    try {
+         if(req.user.accountType != "Instructor"){
+            return res.status(401).json({
+                success: false,
+                message: "This is a protected route for Instructors only"
+            }) 
+         }
+         next()
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "user role cannot be verified"
+        })
+    }
+}
