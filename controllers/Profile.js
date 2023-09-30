7d703feff322196a12d69b8842e2981ca740088c
vaishnav-rajput <vaishnav.rajput108@gmail.com>
2023-09-30 16:07:41 +0530

    delete account controller

--- controllers/Profile.js
+++ controllers/Profile.js
@@ -43,3 +43,60 @@ exports.updateProfile = async (req, res) => {
         })
     }
 }
+
+//delete account
+exports.deleteAccount = async(req, res) => {
+    try {
+        //get Id
+        const id = req.user.id;
+        //validation
+        const userDetails = await User.findById(id)
+        if(!userDetails){
+            return res.status(404).json({
+                success: false,
+                message: "user not found"
+            })
+        }
+        //delete profile
+        await Profile.findByIdAndDelete({_id: userDetails.additionalDetails})
+        //TODO: hw unenroll user from all enrolled courses
+        //delete user
+        await User.findByIdAndDelete({_id: id})
+
+        //return response
+        return res.status(200).json({
+            success: true,
+            message: "User deleted successfully"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "User cannot be deleted"
+        })
+    }
+}
+
+exports.getAllUserDetails = async(req, res) => {
+    try {
+        const id = req.user.id
+
+        const userDetails = await User.findById(id).populate("additionalDetails").exec()
+
+        return res.status(200).json({
+            success: true,
+            message: "User data fetched successfully"
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+
+
+
+
+
